b9a6e260209cf008f43e507e2f542340652b19b9
wadhass <ahmedhassansumu@gmail.com>
2025-04-04 16:51:55 +0200

    index-corrected

--- config/db.js
+++ config/db.js
@@ -17,19 +17,19 @@
 
 // module.exports= connectDb;
 
-
-
-
 const mongoose = require('mongoose');
 
-const connectDb = async() =>{
-      try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('database connection successfully');
-      } catch (error) {
-        console.error('error connecting to database', error.message);
-        process.exit(1);
-      }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Error connecting to database:', error.message);
+    process.exit(1);
+  }
+};
 
-module.exports= connectDb;
+module.exports = connectDb;
