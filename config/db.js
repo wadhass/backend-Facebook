a222ab6c4697080a31b71f7e232343159d6767dd
wadhass <ahmedhassansumu@gmail.com>
2025-04-04 15:14:32 +0200

    server-corrected

--- config/db.js
+++ config/db.js
@@ -1,14 +1,31 @@
+// const mongoose = require('mongoose');
+
+
+
+// const connectDb = async() =>{
+//       try {
+//         await mongoose.connect(process.env.MONGO_URI,{
+//             useNewUrlParser:true,
+//             useUnifiedTopology:true
+//         });
+//         console.log('database connection successfully')
+//       } catch (error) {
+//         console.error('error connecting to database', error.message);
+//         process.exit(1);
+//       }
+// }
+
+// module.exports= connectDb;
+
+
+
+
 const mongoose = require('mongoose');
-
-
 
 const connectDb = async() =>{
       try {
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
-        console.log('database connection successfully')
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('database connection successfully');
       } catch (error) {
         console.error('error connecting to database', error.message);
         process.exit(1);
